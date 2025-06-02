420a75b7af8fe7f3b6c33fa725eb834fa4b622f1
Carlos Guillermo <e22081429@itmerida.edu.mx>
2025-06-02 00:01:44 -0600

    Corrijo conexión para Railway

--- Backend-Cerrajeria/config/db.js
+++ Backend-Cerrajeria/config/db.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'mysql.railway.internal',
-  user: 'root',            // El usuario de MySQL en XAMPP
-  password: 'iqsJTxaYDnlgAVIlIfxMlHyzqZaaSfkr',            // Si no le pusiste contraseña, déjalo vacío
-  database: 'railway',  // Tu base de datos
-  port: 3306               // Puerto que usa MySQL en XAMPP (por defecto)
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error de conexión:', err);
+    console.error('❌ Error de conexión a MySQL:', err);
     return;
   }
   console.log('✅ Conectado a MySQL desde Node.js');
