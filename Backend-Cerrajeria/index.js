46b5b9380c29eeebaca4b0ca5f24b823948d80a5
Carlos Guillermo <e22081429@itmerida.edu.mx>
2025-06-01 22:02:01 -0600

    Actualizo index.js para soportar HTML/CSS en Railway

--- Backend-Cerrajeria/index.js
+++ Backend-Cerrajeria/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 
-// ✅ Importa rutas
+// ✅ Importa rutas Soy memo
 const rutas = require('./routes/index');
 
 app.use(cors());
